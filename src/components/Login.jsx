5ffd5136bcd73c32d2909d014efe21c0345844eb
208T1A05I0 <saivijnathitatikonda@gmail.com>
2025-10-29 22:34:57 +0530

    2nd commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const loginData = async () => {
+    const dataToSend = {
+      email,
+      password
+    }
+    const reqOptions = {
+      body : JSON.stringify(dataToSend),
+      method : "POST",
+      headers : {
+        "Content-Type" : "application/json"
+      }
+    }
+    try{
+      const JSONData = await fetch("http://localhost:3456/login",reqOptions);
+      const JSOData = await JSONData.json();
+      console.log("Data received",JSOData.status)
+      alert(JSOData.msg);
+      if(JSOData.status === "success"){
+        navigate("/")
+      }
+    }
+    catch(err){
+      console.log("Data not sent", err)
+    }
+  }
+
   return (
     <div className='signup-div'>
     <div className='logo'  style={{margin:"20px"}}><Link to='/' style={{color:"#D38204"}}>Honey's Boutique</Link></div>
@@ -10,13 +43,13 @@ export const Login = () => {
         <form className='signup-form'>
             <div>
                 <label>Email</label>
-                <input/>
+                <input onChange={(e)=>setEmail(e.target.value)}/>
             </div>
             <div>
                 <label>Password</label>
-                <input/>
+                <input onChange={(e)=>setPassword(e.target.value)}/>
             </div>
-            <button>Login</button>
+            <button onClick={()=>loginData()} type='button'>Login</button>
             <span className='signup-span'>Don't have an account? <Link to='/signup' style={{textDecoration:"underline", color:"#00684a"}}>Signup</Link></span>
         </form>
     </div>
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 export const Signup = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const signupData = async () => {
+        const dataToSend = {
+            name,
+            email,
+            password
+        }
+        const reqOptions = {
+            body : JSON.stringify(dataToSend),
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            }
+        }
+        try{
+            const JSONData = await fetch("http://localhost:3456/signup", reqOptions);
+            const JSOData = await JSONData.json();
+            console.log(JSOData.status)
+            console.log("name", name)
+            console.log("email", email)
+            console.log("password", password)
+            console.log("Response Data",JSOData);
+            console.log("JSON Data", JSONData.status);
+            alert(JSOData.status)
+        }catch(err){
+            console.log("Data is not sent or received", err)
+        }
+    }
+
   return (
     <div className='signup-div'>
     <div className='logo'  style={{margin:"20px"}}><Link to='/' style={{color:"#D38204"}}>Honey's Boutique</Link></div>
@@ -10,17 +43,17 @@ export const Signup = () => {
             <form className='signup-form'>
                 <div>
                     <label>Name</label>
-                    <input/>
+                    <input onChange={(e)=>setName(e.target.value)}/>
                 </div>
                 <div>
                     <label>Email</label>
-                    <input/>
+                    <input onChange={(e)=>setEmail(e.target.value)}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input/>
+                    <input onChange={(e)=>setPassword(e.target.value)}/>
                 </div>
-                <button>Signup</button>
+                <button onClick={()=>signupData()} type='button'>Signup</button>
                 <span className='signup-span'>Already have an account? <Link to='/login' style={{textDecoration:"underline", color:"#00684a"}}>Login</Link></span>
             </form>
         </div>
